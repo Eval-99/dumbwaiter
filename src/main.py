85d70e04d9c10906027bc93face1a29462949ffa
Eval-99 <ev1914jw@gmail.com>
2026-01-11 22:49:08 -0600

    Added basic search bar example from docs.

--- src/main.py
+++ src/main.py
@@ -1,25 +1,59 @@
 import flet as ft
 
+colors = [
+    "Amber",
+    "Blue Grey",
+    "Brown",
+    "Deep Orange",
+    "Green",
+    "Light Blue",
+    "Orange",
+    "Red",
+]
+
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
+    def build_tiles(items: list[str]) -> list[ft.Control]:
+        return [
+            ft.ListTile(
+                title=ft.Text(item),
+                data=item,
+                on_click=handle_tile_click,
+            )
+            for item in items
+        ]
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
+    async def handle_tile_click(e: ft.Event[ft.ListTile]):
+        await anchor.close_view()
+
+    async def handle_change(e: ft.Event[ft.SearchBar]):
+        query = e.control.value.strip().lower()
+        matching = (
+            [color for color in colors if query in color.lower()] if query else colors
         )
+        anchor.controls = build_tiles(matching)
+
+    def handle_submit(e: ft.Event[ft.SearchBar]):
+        print(f"Submit: {e.data}")
+
+    async def handle_tap(e: ft.Event[ft.SearchBar]):
+        await anchor.open_view()
+
+    page.add(
+        anchor := ft.SearchBar(
+            view_elevation=4,
+            divider_color=ft.Colors.AMBER,
+            bar_hint_text="Search colors...",
+            view_hint_text="Choose a color from the suggestions...",
+            on_change=handle_change,
+            on_submit=handle_submit,
+            on_tap=handle_tap,
+            controls=build_tiles(colors),
+        ),
     )
 
 
-ft.run(main)
+if __name__ == "__main__":
+    ft.run(main)
